c1817b830aea47aa8ef7eea03786ba33840e0a38
Oleg Yamnikov <bgeyts667@gmail.com>
2016-09-11 15:10:29 +0500

    [integer_partitions_haskell] Add initial version of solution

---
+++ integer_partitions_haskell/intpart.hs
@@ -0,0 +1,30 @@
+import Test.HUnit
+
+-- |Finds all partitions of `n` of length `l`, whose first element is `i`.
+partsOfLen' :: Integer -> Integer -> Integer -> [[Integer]]
+partsOfLen' n l i = ([i]++) `map` partsOfLen (n-i) (l-1)
+
+-- |Finds all partitions of `n` of length `l`.
+partsOfLen :: Integer -> Integer -> [[Integer]]
+partsOfLen n 1 = [[n]]
+partsOfLen n l = foldl (++) [] $ map (partsOfLen' n l) [1..quot n l]
+
+assertPartsOfLen res n l =
+  assertEqual ("for (partsOfLen "++(show n)++" "++(show l)++")") res (partsOfLen n l)
+
+testPartsOfLen =
+  test [ assertPartsOfLen [[5]] 5 1,
+         assertPartsOfLen [[1, 4], [2, 3]] 5 2,
+         assertPartsOfLen [[1, 1, 3], [1, 2, 2]] 5 3,
+         assertPartsOfLen [[1, 1, 1, 2]] 5 4,
+         assertPartsOfLen [[1, 1, 1, 1, 1]] 5 5 ]
+
+-- |Finds all partitions of `n`.
+partitions :: Integer -> [[Integer]]
+partitions n = foldl (++) [] $ map (partsOfLen n) [1..n]
+
+assertPartitions res n =
+  assertEqual ("for (partitions "++(show n)++")") res (partitions n)
+
+testPartitions =
+  test [ assertPartitions [[5], [1, 4], [2, 3], [1, 1, 3], [1, 2, 2], [1, 1, 1, 2], [1, 1, 1, 1, 1]] 5 ]
